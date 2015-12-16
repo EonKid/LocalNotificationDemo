cc0a127c69b652cb440635674698699038a97fa2
Dhruv Narayan Singh <dhruvnarayan1023@gmail.com>
2015-12-16 14:39:19 +0530

    Initial Commit

---
+++ LocalNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LocalNotificationDemo
+//
+//  Created by Aseem 1 on 16/12/15.
+//  Copyright (c) 2015 codeBrew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
